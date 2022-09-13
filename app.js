d18e1cd86bb37df0de44d0739ccfec31c88a2656
Agustin Boccio <aboccio@hotmail.com>
2022-09-13 21:13:22 +0200

    Use cors and express

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const mainRouter = require("./router/mainRouter");
+const cors = require("cors");
 
 app.listen(process.env.PORT || 3030, () => console.log('Server running'));
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
 
 app.use('/', mainRouter);
