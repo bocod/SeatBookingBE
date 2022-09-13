8b3fa1b77be6665e0de04d06d129dcb1e1f62945
Agustin Boccio <aboccio@hotmail.com>
2022-09-13 21:15:56 +0200

    Controller for booking one or several seats at once

--- controller/mainController.js
+++ controller/mainController.js
@@ -6,11 +6,42 @@ const buyTicketPath = path.join(__dirname, '../db/trxTicketBuy.json');
 const buyTicketText = fs.readFileSync(buyTicketPath, 'utf-8');
 const ticketData = JSON.parse(buyTicketText);
 
+function saveSeatChanges(){
+    const ticketDataStringified = JSON.stringify(ticketData);
+    fs.writeFileSync(buyTicketPath, ticketDataStringified, 'utf-8'); 
+};
+
+function symmetricDifference(availableSeats, seatsReq) {
+    let _difference = new Set(availableSeats)
+    for (let elem of seatsReq) {
+      if (_difference.has(elem)) {
+        _difference.delete(elem)
+      } else {
+        _difference.add(elem)
+      }
+    }
+    return ticketData[1].Asientos.Disponibles = [...new Set(_difference)];
+};
+
 module.exports = {
     getFilmData: (req, res) => {
         res.send(ticketData[0].Pelicula);
     },
     getSeatsData: (req, res) => {
         res.send(ticketData[1].Asientos);
-    }
+    },
+    bookSeats: (req, res) => {
+        const seatsReq = req.body.seats;
+        const availableSeats = ticketData[1].Asientos.Disponibles;
+        const seatFound = seatsReq.map((seatReq) => {
+            return availableSeats.find(avSeat => avSeat === seatReq)
+        });
+        if(seatFound.includes(undefined)){
+            return res.status(404).send('Sorry one or more seats you are trying to book are now unavailable...')
+        } else {
+            symmetricDifference(availableSeats, seatsReq);
+            saveSeatChanges();
+            res.status(200).send(`Your seats n. ${seatsReq} have been successfully booked! Don't forget to buy popcorn and enjoy the movie!`)
+        };
+    },
 }
