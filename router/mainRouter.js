7a4afc9f82a3d474202927f9205375b35c3d24ae
Agustin Boccio <aboccio@hotmail.com>
2022-09-14 18:36:36 +0200

    More semantic route name

--- router/mainRouter.js
+++ router/mainRouter.js
@@ -4,6 +4,6 @@ const mainController = require('../controller/mainController');
 
 router.get('/film-data', mainController.getFilmsData);
 router.get('/seats-data', mainController.getSeatsData);
-router.post('/book-seat', mainController.bookSeats);
+router.post('/seats-booking', mainController.bookSeats);
 
 module.exports = router;
